26ec7b14e36e0423a1709a077dad71d7b4a06315
tainahcarvalho21 <tainah_carvalho@hotmail.com>
2023-06-06 23:33:09 -0300

    comandos

--- crud.py
+++ crud.py
@@ -1,14 +1,40 @@
 import mysql.connector 
 
-conexao = mysql.connector.connect(
+connection = mysql.connector.connect(
     host='localhost',
     user='root',
     password= '',
     database='bdcrud',
 )
 
-cursor = conexao.cursor()
+cursor = connection.cursor()
+
+#Create
+#produto = ""
+#valor = 
+new_product = f'INSERT INTO vendas (produto, valor) VALUES ("{produto}", {valor})'
+cursor.execute(new_product)
+connection.commit()
+
+#Read
+buscar_produtos = f'SELECT * FROM vendas'
+cursor.execute(buscar_produtos)
+produtos = cursor.fetchall()
+print(produtos)
+
+#Update
+#produto = ""
+#valor = 
+update = f'UPDATE vendas SET valor = {valor} WHERE produto = "{produto}"'
+cursor.execute(update)
+connection.commit()
+
+#Delete
+#produto = ""
+delete = f'DELETE FROM vendas WHERE produto = "{produto}"'
+cursor.execute(delete)
+connection.commit()
 
 
 cursor.close()
-conexao.close()
+connection.close()
